d2323fb23a5fb36fb7c4ff174286d93385da6bd4
AdamBizan <bizan.adam@gmail.com>
2018-10-24 16:06:43 +0200

    v0.1

---
+++ js/app.js
@@ -0,0 +1,29 @@
+function getQuote(text, author) {
+    var forismaticURL = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?"
+    $.getJSON(forismaticURL, function(data) {
+        $(".quote").html("\"" + data.quoteText + "\"");
+        if (data.quoteAuthor == "") {
+            $(".author").html(" -Anonymous");
+        } else {
+            $("div.author").html(" -" + data.quoteAuthor);
+        }
+    })
+};
+
+var author = $("#author");
+var text = $("#quote");
+
+getQuote(text, author);
+
+    var param1 = encodeURIComponent($("#quote").text());
+    var param2 = encodeURIComponent($("#author").text());
+    var newUrl = "https://twitter.com/intent/tweet?text=" + param1 + param2;
+    // document.getElementById("tweet").attr("href", newUrl);
+
+
+// $("#tweet").click(function() {
+//     var param1 = encodeURIComponent($("#quote").text());
+//     var param2 = encodeURIComponent($("#author").text());
+//     var newUrl = "https://twitter.com/intent/tweet?text=" + param1 + param2;
+//     $(".btn-twitter").attr("href", newUrl);
+// });
